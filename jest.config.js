60843b2f58470f2f9ab3f37cd44be6a9ffe1e7cc
Ankit Kumar <ankit.kumar@financialforce.com>
2023-08-31 16:12:39 +0530

    Add the jest config

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ const { jestConfig } = require('@salesforce/sfdx-lwc-jest/config');
 
 module.exports = {
     ...jestConfig,
+    roots: ['<rootDir>/force-app/main/'],
     modulePathIgnorePatterns: ['<rootDir>/.localdevserver']
 };
