a95d56382f0ca221dff28e101ebedf131b4c1c98
Ankit Kumar <ankit.kumar@financialforce.com>
2023-09-04 00:06:31 +0530

    Added test for the clear message functionality

--- force-app/main/default/lwc/memberSkills/__tests__/memberSkills.test.js
+++ force-app/main/default/lwc/memberSkills/__tests__/memberSkills.test.js
@@ -2,6 +2,7 @@ import { createElement } from 'lwc';
 import MemberSkills from 'c/memberSkills';
 import { createRecord } from 'lightning/uiRecordApi';
 import { teamsTestData } from './data';
+import { ClearMessageEvent } from '../../message/event';
 
 jest.mock('lightning/uiRecordApi', () => ({
     createRecord: jest.fn(),
@@ -75,6 +76,52 @@ describe('c-member-skills', () => {
         const wait = () => {
             return new Promise(resolve => setTimeout(resolve, 1000));
         }
+        await wait();
+        expect(element).toMatchSnapshot();
+    });
+
+    it('should clear the success message after the given time', async () => {
+        // GIVEN
+        const memberInformation = {
+            Name: 'Test Member Name',
+            Skills__c: 'Test skills',
+            Team__c: `${teamsTestData[1].id}`
+        }
+        const element = createElement('c-member-skills', {
+            is: MemberSkills,
+        });
+        element.teams = teamsTestData;
+        document.body.appendChild(element);
+
+        createRecord.mockResolvedValue({
+            fields: {
+                Name: { value: memberInformation.Name },
+            },
+        });
+
+        // WHEN
+        const form = element.shadowRoot.querySelector('form');
+        const input = element.shadowRoot.querySelectorAll('input');
+        const teamSelection = element.shadowRoot.querySelector('select');
+        console.log('input', input);
+        input[0].value = memberInformation.Name;
+        input[1].value = memberInformation.Skills__c;
+        teamSelection.value = memberInformation.Team__c;
+        form.dispatchEvent(new CustomEvent('submit'));
+
+        // THEN
+        expect(createRecord).toHaveBeenCalledWith({
+            apiName: 'TeamMember__c',
+            fields: memberInformation
+        });
+
+        const wait = () => {
+            return new Promise(resolve => setTimeout(resolve, 1000));
+        }
+        await wait();
+
+        const messageComponent = element.shadowRoot.querySelector('c-message');
+        messageComponent.dispatchEvent(new ClearMessageEvent());
         await wait();
         expect(element).toMatchSnapshot();
     });
