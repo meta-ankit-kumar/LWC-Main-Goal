7b64036e4cada0ec0a353005a87ed7ff2d9f5e69
Ankit Kumar <ankit.kumar@financialforce.com>
2023-09-03 23:41:20 +0530

    Added the unit tests for the memberSkills component

---
+++ force-app/main/default/lwc/memberSkills/__tests__/data.js
@@ -0,0 +1,12 @@
+export const teamsTestData = [
+  {
+    id: 1,
+    name: "Team 1",
+    description: "Team 1 description"
+  },
+  {
+    id: 2,
+    name: "Team 2",
+    description: "Team 2 description"
+  }
+];
